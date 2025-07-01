6c2b35e6c4b5a0d18c14205ca5575c1c8e4bce97
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-07-01 14:37:14 +0700

    song controller implemented

--- controller/songs/songs_controller_impl.go
+++ controller/songs/songs_controller_impl.go
@@ -6,6 +6,7 @@ import (
 	"login-app/model/web/songs"
 	service "login-app/service/songs"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -25,6 +26,66 @@ func (controller *SongsControllerImpl) Create(w http.ResponseWriter, r *http.Req
 	helper.ReadFromRequestBody(r, &songCreateRequest)
 
 	songResponse := controller.SongsService.Create(r.Context(), songCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   songResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
+
+func (controller *SongsControllerImpl) Update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	songUpdateRequest := songs.SongUpdateRequest{}
+	helper.ReadFromRequestBody(r, &songUpdateRequest)
+
+	songId := p.ByName("songId")
+	id, err := strconv.Atoi(songId)
+	helper.PanicIfError(err)
+
+	songUpdateRequest.Id = id
+
+	songResponse := controller.SongsService.Update(r.Context(), songUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   songResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
+
+func (controller *SongsControllerImpl) Delete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	songId := p.ByName("songId")
+	id, err := strconv.Atoi(songId)
+	helper.PanicIfError(err)
+
+	controller.SongsService.Delete(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
+
+func (controller *SongsControllerImpl) FindById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	songId := p.ByName("songId")
+	id, err := strconv.Atoi(songId)
+	helper.PanicIfError(err)
+
+	songResponse := controller.SongsService.FindById(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   songResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
+
+func (controller *SongsControllerImpl) FindAll(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	songResponse := controller.SongsService.FindAll(r.Context())
 	webResponse := web.WebResponse{
 		Code:   200,
 		Status: "OK",

e6ddaf2eab4801e55291c819fa06d8dcb9526f27
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-07-01 14:24:26 +0700

    update song controller

--- controller/songs/songs_controller_impl.go
+++ controller/songs/songs_controller_impl.go
@@ -1,6 +1,14 @@
 package songs
 
-import service "login-app/service/songs"
+import (
+	"login-app/helper"
+	"login-app/model/web"
+	"login-app/model/web/songs"
+	service "login-app/service/songs"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 type SongsControllerImpl struct {
 	SongsService service.SongsService
@@ -10,4 +18,18 @@ func NewSongsController(songsService service.SongsService) *SongsControllerImpl 
 	return &SongsControllerImpl{
 		SongsService: songsService,
 	}
+}
+
+func (controller *SongsControllerImpl) Create(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	songCreateRequest := songs.SongCreateRequest{}
+	helper.ReadFromRequestBody(r, &songCreateRequest)
+
+	songResponse := controller.SongsService.Create(r.Context(), songCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   songResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
